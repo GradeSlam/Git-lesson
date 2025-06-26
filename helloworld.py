29fddae728f98d24d3feea758b8ab09564eb20c9
Enrico De Flaviis <23deflaviienrico@gmail.com>
2025-06-25 21:02:27 -0700

    median function

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,13 @@
-print("goodbye world")
+import math
+
+def median(input):
+  if len(input) % 2 == 0:
+    top_middle = len(input) // 2
+    bottom_middle = top_middle - 1
+    return (input[bottom_middle] + input[top_middle]) / 2
+  else:
+    middle = len(input) // 2
+    return input[middle]
+
+my_list = [0, 1, 2, 3, 4, 5]
+print(median(my_list))
